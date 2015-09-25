468d1a9e3d52b7ac34ec0bcc159399a63c781863
xcomar <marcohm15@hotmail.com>
2015-09-25 18:30:33 -0500

    Initial commit

---
+++ app/src/main/java/com/servicemarketpanama/smamobiledroid/activities/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.servicemarketpanama.smamobiledroid.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.servicemarketpanama.smamobiledroid.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(10000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
